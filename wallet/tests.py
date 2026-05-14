790d4d95d89c20b088330f0d0d61808aceb9c095
mahkamovmuzaffar <maxalphaengineer@gmail.com>
2026-05-14 06:08:30 +0500

    Test

--- wallet/tests.py
+++ wallet/tests.py
@@ -3,3 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 # TODO: Add tests for wallet functionality, such as creating a wallet, adding funds, and making transactions.
 # TODO: Test edge cases, such as insufficient funds and invalid transactions.   
+# TODO: Use Django's testing framework to create unit tests for the wallet app. 
